215feebaef72ef4279334cb486a421630b604ef3
Henry Herrera <henryjherreram@gmail.com>
2020-09-18 12:25:09 -0700

    finding bugs

--- src/Earthquake.js
+++ src/Earthquake.js
@@ -10,14 +10,15 @@ export class Earthquake extends Component {
         maxMagnitude: "",
         countries: ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"],
         country: "",
-        earthquakeData: []
+        earthquakeData: [],
+        show: false
     }
 
     onChange = e => {
         this.setState({
             [e.target.name]: e.target.value
         });
-        console.log(e.target.value);
+        // console.log(e.target.value);
     };
 
     onClick() {
@@ -30,17 +31,24 @@ export class Earthquake extends Component {
                             earthquakeData: data
                         });
                     }
-                    console.log(data);
+                    // console.log(data.features.length);
                 })
                 .catch(error => {
                     console.log(`there is an error ${error}`);
                 });
         });
+
+        this.setState({
+            show: true
+        });
+
     }
 
     render() {
         const magnitudes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        console.log(this.state.earthquakeData);
+
+        console.log(this.state.earthquakeData.features);
+
         return (
             <div>
                 <input name="startDate" onChange={this.onChange} type="date" />
@@ -70,7 +78,20 @@ export class Earthquake extends Component {
 
                 <br />
 
-                <button onClick={this.onClick}>Search</button>
+                <button onClick={() => this.onClick()}>Search</button>
+
+                {this.state.show ? this.earthquakeData ? this.earthquakeData.features.filter(data =>
+                    data.properties.place.includes(this.state.country) &&
+                    data.properties.mag <= this.state.maxMagnitude &&
+                    data.properties.mag >= this.state.minMagnitude
+                    && data.properties.time.toLocaleString() >= this.state.startDate &&
+                    data.properties.time.toLocaleString() >= this.state.endDate)
+                    .map((earthquake, index) => (
+                        <div key={index}>
+                            <p>{earthquake.properties.name}</p>
+                            <p>{earthquake.properties.mag.toFixed(2)}</p>
+                        </div>
+                    )) : "Nothing to show" : ""}
 
             </div>
         )
